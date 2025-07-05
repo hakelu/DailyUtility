48143c01689baaac769ae01f0c734680eeddb855
Hake Lu <hake.lu@gmail.com>
2025-07-05 17:48:54 +0800

    update toggleScreen.sh

--- toggleScreen.sh
+++ toggleScreen.sh
@@ -7,8 +7,8 @@ HDMI_STATUS="/sys/class/drm/card0-HDMI-A-1/status"
 # Check if HDMI is connected and toggle eDP accordingly
 if [ -f "$HDMI_STATUS" ] && grep -q "connected" "$HDMI_STATUS"; then
     echo "HDMI is connected. Disabling internal display (eDP-1)."
-    echo 0 > /sys/class/drm/card0-eDP-1/enabled
+    echo "off" > /sys/class/drm/card0-eDP-1/status
 else
     echo "HDMI is not connected. Enabling internal display (eDP-1)."
-    echo 1 > /sys/class/drm/card0-eDP-1/enabled
+    echo "on" > /sys/class/drm/card0-eDP-1/status
 fi
